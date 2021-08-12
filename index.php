1234f813d3fbeae664aeb79dac0bc6d05a46e86d
Zamandastgir <47104108+Zamandastgir@users.noreply.github.com>
2021-08-12 16:37:49 +0500

    new files

---
+++ index.php
@@ -0,0 +1,291 @@
+
+<html>
+<head>
+					<link rel="stylesheet" type="text/css" href="snipetscroll2.css">
+	<script type="text/javascript" src="js/examples.js"></script>    
+    <script type="text/javascript" src="js/lib/jquery.min.js"></script>
+	<script type="text/javascript" src="js/lib/highlight.pack.js"></script>
+	<script type="text/javascript" src="js/lib/modernizr.custom.min.js"></script>
+	<script type="text/javascript" src="js/lib/greensock/TweenMax.min.js"></script>
+    
+	<script type="text/javascript" src="scrollmagic/uncompressed/ScrollMagic.js"></script>
+	<script type="text/javascript" src="scrollmagic/uncompressed/plugins/animation.gsap.js"></script>
+	<script type="text/javascript" src="scrollmagic/uncompressed/plugins/debug.addIndicators.js"></script>
+      
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+</head>
+<body>
+ <div id="nav">
+ <img src="img/logo.png">
+ 
+ <ul class="home">
+        <li>home</li>
+        <li>services</li>
+        <li>portfolio</li>
+        <li>team</li>
+        <li>blog</li>
+        <li>contact</li>
+        <li><i class="fa fa-search"></i></li>
+  </ul>
+	      
+        <ul class="top-icons">
+        <li><i class="fa fa-facebook"></i></li>
+        <li><i class="fa fa-twitter"></i></li>
+        <li><i class="fa fa-instagram"></i></li>
+        </ul>	
+		
+ 
+ </div>
+<div id="pinContainer">
+	 
+	 <div id="slideContainer">
+	 
+     <section class="panel blue">
+      <div id="main">
+ 
+     <p>WE WORK HARD, WE RESULT PERFECT</p>
+        <h1>CRAFTING DIGITAL AGENCY EXPERIENCES</h1>
+        <button>Explore Us</button>
+ 
+     </div>
+     </section>   <!-- end section blue here -->
+	 	 
+
+       <section class="panel turqoise">
+	
+     <div id="digital">	
+	  <p>ABOUT OUR DIGITAL AGENCY</p>
+	 <h4>Plantronics with its GN Netcom <span style="color:black;">wireless headset</span>
+	 creates the next generation of wireless headset and other products such as wireless amplifiers,
+	 and wireless <span style="color:black;">headset</span> telephone.</h4>
+	 
+	 
+	 <div class="digital-A">
+		      <div class="digital-1">
+            <img src="img/s1.jpg">
+            </div>
+			<h2>ADDICTION WHIT GAMBLING</h2>
+            <p>It is a good idea to think of your PC as an office. 
+			It stores files, programs, pictures. This can be compared to an actual office’s files</p>
+            <button>Explore <i class="fa fa-long-arrow-right"></i></button>
+            
+		</div>
+		
+	     <div class="digital-B">
+		 <div class="digital-1">
+            <img src="img/s2.jpg">
+            </div>
+			<h2>ADDICTION WHIT GAMBLING</h2>
+            <p>It is a good idea to think of your PC as an office. 
+			It stores files, programs, pictures. This can be compared to an actual office’s files</p>
+            <button>Explore <i class="fa fa-long-arrow-right"></i></button>
+            
+		</div> 
+		
+		 <div class="digital-C">
+		    <div class="digital-1">
+            <img src="img/s3.jpg">
+            </div>
+			<h2>ADDICTION WHIT GAMBLING</h2>
+            <p>It is a good idea to think of your PC as an office. 
+			It stores files, programs, pictures. This can be compared to an actual office’s files</p>
+            <button>Explore <i class="fa fa-long-arrow-right"></i></button>
+            
+		</div>
+
+
+    
+</div>
+   
+    </section>  <!-- end section turqoise -->
+	
+		
+		<section class="panel green">
+        <div id="choose">
+                <p>WHY CHOOSE US</p>
+                <h4>We ensure perfect quality Digital products for you</h4>
+				
+				<div id="choose-A">
+                <i class="fa fa-star-o"></i>
+                <h3>Unique Design</h3>
+                <p>Most people who work in an office environment, buy computer products, or have</p>
+                </div>
+                
+                <div id="choose-B">
+                <i class="fa fa-magic"></i>
+                <h3>Appropriate UX</h3>
+                <p>Today, many people rely on computers to do homework, work, and create or store useful</p>
+                </div>
+                
+                <div id="choose-C">
+                <i class="fa fa-apple"></i>
+                <h3>Perfect Visual</h3>
+                <p>Having a baby can be a nerve wracking experience for new parents – not the nine months</p>
+                </div>
+                
+                <div id="choose-D">
+                <i class="fa fa-database"></i>
+                <h3>Different Layout</h3>
+                <p>It won’t be a bigger problem to find one video game lover in your neighbor. Since the</p>
+                </div>
+                
+        </div>
+</section>   <!-- end sectiton green here -->
+
+
+					<section class="panel bordeaux">
+			<div id="contact">
+                <p>LATEST FROM BLOG</p>
+            
+                <h2>Publish what you want to not requires social</h2>
+                <h3>Dramatically reintermediate effective applications after high-payoff core competencies. Authoritatively optimize collaborative benefits for error-free communities. Dramatically reintermediate effective applications after high-payoff core competencies. Authoritatively optimize collaborative benefits for error-free communities.</h3>
+            
+             <ul class="phy">
+                <li><b>PHYSICAL ADDRESS</b></li>
+                <li><b>WEB CONTACT</b></li>
+            </ul>
+            
+            <ul class="num">
+                <li>Emperor Publishing,<br>56/8, bir uttam qazi nuruzzaman <br>road, Dhaka - 1205</li>
+                <li>012 (88) 017 25 656 565 <br>012 (88) 017 25 656 565</li>
+            </ul>
+            
+			
+			<input type="text" placeholder="Your Full Name" class="name">
+            <br>
+            <br>
+            <input type="email" placeholder="Your Email Address" class="email">
+            <br>
+            <br>
+            <input type="text" placeholder="Subject" class="sub">
+            <br>
+            <br>
+            <input type="text" placeholder="Type Your Massage Here" class="area">
+            
+            </div>
+		</section>  <!-- end section bordeaux here -->
+		
+        
+		<section class="panel blue2">
+			<div id="about-us">
+            <div id="about-A">
+            <div class="about-A">
+                <h1>ABOUT AGENCY</h1>
+                <p>The world has become so fast paced that people don’t want to stand by reading a page of information, they would much rather look at a presentation and understand the message. It has come to a point</p>
+                </div>
+				
+			  <div class="skill">
+                    <h1>NAVIGATION LINKS</h1>
+                    <ul class="seri">
+                        <li>Home</li>
+                        <li>Featuers</li>
+                        <li>Services</li>
+                        <li>Portfolio</li>
+                    </ul>
+                    
+                    <ul class="blog">
+                        <li>Team</li>
+                        <li>Pricing</li>
+                        <li>Blog</li>
+                        <li>Contact</li>
+                    </ul>
+                </div>
+				
+				
+				<div class="news">
+                    <h1>NEWSLETTER</h1>
+                    <p>For business professionals caught between high OEM price and mediocre print and graphic output.</p>
+                    
+                    <input type="email" placeholder="Email address" class="arrow">
+                <div class="nav-arrow">
+                   <i class="fa fa-external-link"></i>
+                    </div>
+                </div>
+				
+               <div class="insta">
+                <h1>INSTAFEED</h1>
+                <div id="imga">
+                <img src="img/i1.jpg">
+                <img src="img/i2.jpg">
+                <img src="img/i3.jpg">
+                <img src="img/i4.jpg">
+                <img src="img/i5.jpg">
+                <img src="img/i6.jpg">
+                <img src="img/i7.jpg">
+                <img src="img/i8.jpg">
+                </div>
+                </div>
+                 
+	      </div>
+		  
+		  <div id="about-B">
+                <p>Copyright © 2017 | All rights reserved to Datarc inc. Designed by Colorlib.</p>
+                
+                <ul>
+                    <li><i title="Facebook" class="fa fa-facebook"></i></li>
+                    <li><i title="Twitter" class="fa fa-twitter"></i></li>
+                    <li><i title="Dribbble" class="fa fa-dribbble"></i></li>
+                    <li><i title="Behance" class="fa fa-behance"></i></li>
+                </ul>
+                
+                
+                </div>
+		  
+		  
+		  
+		  
+            
+            </div>
+		</section>
+        
+
+	
+	
+		 
+ 
+      </div> <!-- end pinContainer here -->
+</div>  <!-- end slideContainer here -->
+
+
+
+
+ <script>
+	$(function () { // wait for document ready
+		// init
+		var controller = new ScrollMagic.Controller();
+
+		// define movement of panels
+		var wipeAnimation = new TimelineMax()
+			// animate to second panel
+			.to("#slideContainer", 0.5, {z: -2000})		// move back in 3D space
+			.to("#slideContainer", 1,   {x: "-20%"})	// move in to first panel
+			.to("#slideContainer", 0.5, {z: 0})				// move back to origin in 3D space
+		    // animate to third panel
+			.to("#slideContainer", 0.5, {z: -2000, delay: 1})
+			.to("#slideContainer", 1,   {x: "-40%"})
+			.to("#slideContainer", 0.5, {z: 0})
+			// animate to forth panel
+			.to("#slideContainer", 0.5, {z: -2000, delay: 1})
+			.to("#slideContainer", 1,   {x: "-60%"})
+			.to("#slideContainer", 0.5, {z: 0})
+             // animate to fifth panel
+            .to("#slideContainer", 0.5, {z: -2000, delay:1})
+            .to("#slideContainer", 1,   {x:"-80%"})
+            .to("#slideContainer", 0.5, {z:0});            
+		// create scene to pin and link animation
+		new ScrollMagic.Scene({
+				triggerElement: "#pinContainer",
+				triggerHook: "onLeave",
+				duration: "500%"
+			})
+			.setPin("#pinContainer")
+			.setTween(wipeAnimation)
+			.addTo(controller);
+	});
+</script>
+  
+</body>
+
+</html>
